892756efcfb47fbc4eb40a1de7b644bd56093d7f
K-SunilKumar-18 <sunilkiladi2003@gmail.com>
2025-06-22 14:49:14 +0530

    fir

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' for React 18+
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // Create a root element
+root.render(
+  <App /> // Render the App component
+);
